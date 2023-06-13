5735194c1a2c6292130a7e66d18dc10a2a0e056b
Kathiravanmohan <kathiravanmohan19@gmail.com>
2023-06-13 21:57:28 +0530

    Shopify-card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+ import Header from "./Component/header";
+ import Navbar from "./Component/Navbar";
+import Card from "./Component/card";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [cartValue,setCartvalue]= useState(0)
+  return <>
+  <Navbar cartValue={cartValue }/>
+  <Header/>
+  <section className="py-5">
+            <div className="container px-4 px-lg-5 mt-5">
+                <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                  <Card setCartvalue={setCartvalue}/>
+                  <Card setCartvalue={setCartvalue}/>
+                  <Card setCartvalue={setCartvalue}/>
+                  <Card setCartvalue={setCartvalue}/>
+                  <Card setCartvalue={setCartvalue}/>
+          
+                  </div>
+                </div>
+                </section>
+  </>
 }
 
 export default App;
